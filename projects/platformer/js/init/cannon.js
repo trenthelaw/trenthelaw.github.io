303ad7937dabdb2c9bfabd633ec2dcfddf5633dc
trenthelaw <law29574@students.apsb.org>
2019-09-30 22:47:02 +0000

    save all

---
+++ projects/platformer/js/init/cannon.js
@@ -0,0 +1,36 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.collectable = window.opspark.collectable || {};
+    let cannon = window.opspark.cannon;
+    
+    /**
+     * init: Initialize all cannons.
+     * 
+     * Add as many cannons as necessary (at least 3) to make your level challenging. 
+     *
+     * The following functions are available to you:
+     *  cannon.create.onTop(xLocation);
+     *  cannon.create.onBottom(xLocation);
+     *  cannon.create.onLeft(yLocation);
+     *  cannon.create.onRight(yLocation);
+     */ 
+    cannon.init = function (game) {
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        // example: 
+        cannon.create.onTop(450);
+        cannon.create.onTop(100)
+        cannon.create.onTop(200)
+        cannon.create.onTop(300)
+        cannon.create.onTop(400)
+        cannon.create.onTop(609)
+        cannon.create.onTop(69)
+        cannon.create.onTop(550)
+        cannon.create.onTop(650)
+        
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    };
+})(window);
